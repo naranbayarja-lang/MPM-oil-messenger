a6919caf658c23e6ca7c4c0dd0c67630a3d95024
naranbayarja-lang <naranbayar.ja@gmail.com>
2026-04-11 03:04:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import express from "express";
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "mpm2026";
 
-const app = express();
-app.use(express.json());
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
-app.get("/", (req, res) => {
-  res.send("MPM chatbot ажиллаж байна 🚀");
-});
-
-app.listen(3000, () => {
-  console.log("Server started");
+  if (mode && token === VERIFY_TOKEN) {
+    console.log("Webhook verified");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
 });
